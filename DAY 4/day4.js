d756bbf2653413fc46375e581a04a60a36d28203
vileroze <50352881+vileroze@users.noreply.github.com>
2022-12-29 10:57:16 +0545

    slight changes in bill calculator

--- DAY 4/day4.js
+++ DAY 4/day4.js
@@ -3,11 +3,12 @@ function billCalc(){
 
     //get units
     let units = document.getElementById("units").value;
-    let price_per_unit = 0; 
+    let ppu = 0; 
 
-    //check if element is empty
+    //check if units empty
     if(units===null || units==="") return alert("Units cannot be empty !!");
 
+    //calculate price per unit
     if(units<=115){
         ppu = 9;
     }else if(units>115 && units<=125){
@@ -16,8 +17,7 @@ function billCalc(){
         ppu = 15;
     }
 
-    
     //display result
-    let result = "Your total bill is: " + units + "units X Rs" + ppu + " = Rs" + units*ppu;
+    let result = "Your total bill is: " + units + "units X Rs." + ppu + " = Rs." + units*ppu;
     document.getElementById("bill-result").innerHTML = result;
 }
